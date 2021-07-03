f02e099f6396683c4c44aa12591314471f20ebb4
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-07-03 12:52:46 +0200

    [UP] json and http library

--- KotlinRequestApi/app/src/main/java/com/taccarlo/kotlinrequestapi/MainActivity.kt
+++ KotlinRequestApi/app/src/main/java/com/taccarlo/kotlinrequestapi/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.taccarlo.kotlinrequestapi
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,30 @@ class MainActivity : AppCompatActivity() {
         val rView: RecyclerView = findViewById(R.id.recyclerView_main)
             rView.layoutManager = LinearLayoutManager(this)
             rView.adapter = MainAdapter()
+            fetchJson()
+    }
+    fun fetchJson(){
+        println("Attempting to fetch JSON")
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println("URL response:$body")
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+            }
+        })
     }
 }
 
+class HomeFeed( val videos: List<ListItem>)
+class ListItem(val id: Int, val name: String, val link:String, val imageUrl: String, numberOfViews: String,
+                val channel:Channel)
+class Channel(val name: String, val profileimageUrl:String)
